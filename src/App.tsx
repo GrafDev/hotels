874c1cebd62198277071d22aaa4dc8d13eca6637
GrafDev <gregory.yakovlev@gmail.com>
2024-11-04 14:09:25 +0100

    Start

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import {Button} from "./components/ui/button.tsx";
+import {Box} from "@chakra-ui/react";
+import {Switch} from "./components/ui/switch.tsx";
+
+function App() {
+
+    return (
+        <div>
+            Hotels test.
+            <Box height={2}>
+                <Switch >Label</Switch>
+            <Button>
+                Задача 1
+            </Button>
+            <Button>
+                Задача 1
+            </Button>
+            <Button>
+                Задача 1
+            </Button>
+            <Button>
+                Задача 1
+            </Button>
+            </Box>
+        </div>
+    )
+}
+
+export default App
